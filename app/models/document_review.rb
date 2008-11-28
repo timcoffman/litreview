c143e0898c3ca47852a2abf1b0e0df9a0888afdc
Tim Coffman <timcoffman@mac.com>
2008-11-28 17:49:57 -0600

    corrected logic of following_ and preceding_siblings of document reviews

--- app/models/document_review.rb
+++ app/models/document_review.rb
@@ -1,10 +1,24 @@
 class DocumentReview < ActiveRecord::Base
+	before_save :add_disposition_timestamp
+	
 	belongs_to :document
 	belongs_to :stage_reviewer
 	has_many :document_review_reasons
 	has_many :reasons, :through => :document_review_reasons
-	has_many :preceding_siblings, :class_name => 'DocumentReview', :finder_sql => 'SELECT * FROM document_reviews WHERE (reviewer_sequence + reviewer_snooze) <= #{reviewer_sequence + reviewer_snooze} AND id <> #{id} ORDER BY (reviewer_sequence + reviewer_snooze) DESC, id DESC'
-	has_many :following_siblings, :class_name => 'DocumentReview', :finder_sql => 'SELECT * FROM document_reviews WHERE (reviewer_sequence + reviewer_snooze) > #{reviewer_sequence + reviewer_snooze} ORDER BY (reviewer_sequence + reviewer_snooze) ASC, id ASC'
+	has_many :preceding_siblings, :class_name => 'DocumentReview', :finder_sql => 'SELECT * FROM document_reviews WHERE stage_reviewer_id = #{stage_reviewer_id} AND ((reviewer_sequence + reviewer_snooze) < #{reviewer_sequence + reviewer_snooze} OR ((reviewer_sequence + reviewer_snooze) = #{reviewer_sequence + reviewer_snooze} AND id < #{id})) ORDER BY (reviewer_sequence + reviewer_snooze) DESC, id DESC'
+	has_many :following_siblings, :class_name => 'DocumentReview', :finder_sql => 'SELECT * FROM document_reviews WHERE stage_reviewer_id = #{stage_reviewer_id} AND ((reviewer_sequence + reviewer_snooze) > #{reviewer_sequence + reviewer_snooze} OR ((reviewer_sequence + reviewer_snooze) = #{reviewer_sequence + reviewer_snooze} AND id > #{id})) ORDER BY (reviewer_sequence + reviewer_snooze) ASC, id ASC'
+	
+	def when_assigned
+		self.created_at
+	end
+	
+	def when_assigned=( datetime )
+		self.created_at = datetime
+	end
+	
+	def add_disposition_timestamp
+		self.when_reviewed ||= Time.now if self.attribute_present?(:disposition)
+	end
 	
 	def includes_reason?( reason )
 		return self.reasons.include?( reason )
