cf2f3c7703c620e4ea2016fc1a1f62047df943d5
Tim Coffman <timcoffman@mac.com>
2011-01-19 15:36:14 -0600

    another batch of misc changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,4 +16,4 @@ config.action_controller.perform_caching             = false
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 
-#ActionController::AbstractRequest.relative_url_root = "/litreview-dev"
+ActionController::AbstractRequest.relative_url_root = "/litreview-dev"
