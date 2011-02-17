4855b6c671f670c2451bf8593235c80fb46e963a
Tim Coffman <timcoffman@mac.com>
2011-02-17 14:42:03 -0600

    removed migration files

--- import.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# convert line continuations into continued lines
-perl -e 'while(<>) { s/\\\r?\n$/ /; print ; }' < import.sql > import.c.sql
-/usr/local/mysql/bin/mysql --user litreview --password < import.c.sql
