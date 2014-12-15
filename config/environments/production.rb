9fcab5bbd417cec4da2626a56618b34be3765765
Tim Coffman <timcoffman@mac.com>
2014-12-15 10:14:46 -0600

    removed google OpenID link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,3 +22,14 @@ config.action_view.cache_template_loading            = true
 # config.action_mailer.raise_delivery_errors = false
 
 #ActionController::AbstractRequest.relative_url_root = "/litreview"
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
+
