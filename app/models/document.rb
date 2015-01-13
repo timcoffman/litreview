1c72f843df9b92b74820a3aaf5864d8e6a26550c
Tim Coffman <timcoffman@mac.com>
2015-01-13 13:13:38 -0600

    github-2 added truncation of document fields which exceed database column limits; also converting empty strings to null

--- app/models/document.rb
+++ app/models/document.rb
@@ -6,4 +6,22 @@ class Document < ActiveRecord::Base
 	belongs_to :duplicate_of_document, :class_name => 'Document', :foreign_key => :duplicate_of_document_id
 	has_many :document_tags, :dependent => :delete_all
 	has_many :tags, :through => :document_tags
+	
+	before_save :truncate_values
+	
+	def truncate_values
+		[ :pub_ident, :title, :authors, :journal, :when_published, :abstract ].each do |g|
+			s = (g.to_s + "=").to_sym
+			v = self.send(g)
+			limit = Document.columns_hash[g.to_s].limit
+			if v && limit && v.length > limit
+				v = v[0..(limit-5)] + "..."
+				self.send(s,v) 
+			elsif v && v.empty?
+				self.send(s,nil)
+			end
+		end
+	end
+	
+	
 end
